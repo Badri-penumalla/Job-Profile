48538615e22f285459ea141d546c686688d9cbc2
Badri-penumalla <badri8penumalla@gmail.com>
2025-12-19 18:57:25 +0530

    Day-2

---
+++ src/component/register/InputField.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const InputField = ({type, name, value, handleChange, children}) => {
+  return (
+    <div className={`w-full h-10 flex justify-center items-center relative p-2 group focus-within:border-2 focus-within:rounded-sm ${value?"border-2 rounded-sm":"border-b-2"}`}>
+    
+            <input type={type} id={name} className='size-full outline-0' value={value} name={name} onChange={handleChange}/>
+    
+            <label htmlFor={name} className={`absolute capitalize duration-200 left-3 group-focus-within:-top-3.5 group-focus-within:bg-white group-focus-within:text-[12px] p-1 ${value?"-top-3.5 bg-white text-[12px]":"top-1 left-3"}`}>{name}</label>
+    
+            {children}
+    </div>
+  )
+}
+
+export default InputField
--- src/component/register/Register.jsx
+++ src/component/register/Register.jsx
@@ -1,20 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
+import InputField from './InputField';
+import SelectField from './SelectField'
 import { MdOutlineMail } from "react-icons/md";
+import { AiOutlineMobile } from "react-icons/ai";
+import { MdOutlineDriveFileRenameOutline } from "react-icons/md";
+import { MdOutlinePassword } from "react-icons/md";
+import { LuGraduationCap } from "react-icons/lu";
+import { PiBuildings } from "react-icons/pi";
+import { HiOutlineBuildingLibrary } from "react-icons/hi2";
 
 const Register = () => {
+  const [formData, setFormData] = useState(
+    {
+      name:"",
+      email:"",
+      mobile_no:"",
+      password:"",
+      confirm_password:"",
+      yop:"",
+      institute_name:"",
+      college:"",
+      position_applying:""
+    }
+  )
+  const {email, name, mobile_no, password, confirm_password, yop, institute_name, college, position_applying} = formData;
+  const handleChange = (e) => {
+    let {name, value} = e.target;
+    setFormData((preVal)=>({...preVal, [name]:value}))
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(formData);
+    
+  }
   return (
     <div className='size-full flex justify-center items-center'>
       <div className='w-1/3 h-9/10 rounded-2xl p-10 shadow-2xl'>
-        <form action="" className='size-full'>
+        <form action="" className='size-full flex flex-col gap-3' onSubmit={handleSubmit}>
           <div className='w-full h-10 flex justify-center items-center'>
             <h1 className='text-2xl font-bold'>Register</h1>
           </div>
 
-          <div className='w-full h-10 flex justify-center items-center relative border-b-2 p-2 group focus-within:border-2 focus-within:rounded-sm'>
-            <input type="email" name="" id="email" className='size-full outline-0'/>
-            <label htmlFor="email" className='absolute duration-200 top-1 left-3 group-focus-within:-top-3.5 group-focus-within:bg-white group-focus-within:text-[12px] p-1'>Email</label>
-            <MdOutlineMail />
-          </div>
+          <InputField type="text" name="name" value={name} handleChange={handleChange}><MdOutlineDriveFileRenameOutline /></InputField>
+          <InputField type="email" name="email" value={email} handleChange={handleChange}><MdOutlineMail /></InputField>
+          <InputField type="tel" name="mobile_no" value={mobile_no} handleChange={handleChange}><AiOutlineMobile /></InputField>
+          <InputField type="password" name="password" value={password} handleChange={handleChange}><MdOutlinePassword /></InputField>
+          <InputField type="password" name="confirm_password" value={confirm_password} handleChange={handleChange}><MdOutlinePassword /></InputField>
+          <SelectField position_applying={position_applying} handleChange={handleChange} />
+          <InputField type="number" name="yop" value={yop} handleChange={handleChange}><LuGraduationCap /></InputField>
+          <InputField type="text" name="institute_name" value={institute_name} handleChange={handleChange}><HiOutlineBuildingLibrary /></InputField>
+          <InputField type="text" name="college" value={college} handleChange={handleChange}><PiBuildings /></InputField>
+
+
+          <button type='submit'>Register</button>
         </form>
       </div>
     </div>
